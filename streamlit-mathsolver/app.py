6fd93741f9b6b2857027266c6e900554ae8e912c
Kutlwano Brian Tshatiwa <51665574+BrianTshatiwa@users.noreply.github.com>
2021-10-17 14:36:06 +0200

    streamlit app

--- streamlit-mathsolver/app.py
+++ streamlit-mathsolver/app.py
@@ -1,6 +1,5 @@
 import streamlit as st 
-from .utils.solver import predict
-
+from utils.solver import predict
 
 def get_answer(problem):
     """
@@ -18,4 +17,3 @@ problem = st.text_input(label="Problem here") # prompt user for a math problem
 
 answer = get_answer(problem) # get a predicted answer for the problem
 st.write(f"Predicted answer is: {answer}") # return the predicted answer to the user.
-
